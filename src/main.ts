871c385d82067f8d30f5f5e71b7128e1a3641a69
Micaello <104875687+micaellodev@users.noreply.github.com>
2026-02-13 19:26:23 -0500

    Add localhost:3001 to CORS origins

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
     // Enable CORS for frontend
     // Enable CORS for frontendd
     app.enableCors({
-        origin: ['https://emilianipizzas.com', 'http://localhost:3000', 'https://bira-backend-production.up.railway.app','https://www.emilianipizzas.com'],
+        origin: ['https://emilianipizzas.com', 'http://localhost:3000', 'https://bira-backend-production.up.railway.app','https://www.emilianipizzas.com', 'http://localhost:3001'],
         methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
         credentials: true,
     });
